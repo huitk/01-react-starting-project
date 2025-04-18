a2125e283d0fbc99f38b8cc5b2dc41ed6810ebf2
Tongjit Karawapornphut <hui_naka@hotmail.com>
2025-04-18 01:13:47 -0400

    more code for buttons topic 53

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import reactImg from './assets/react-core-concepts.png';
-import { CORE_CONCEPTS } from './data';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-const description = reactDescriptions[genRandomInt(2)];
-
-  return (<header>
-    <img src={reactImg} alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      {description} React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>);
-}
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
+  let tabContent = 'Please click a button';
+
+  function handleSelect(selectedButton) {
+    //selectedButton => 'components', 'jsx', 'props', 'state'
+    tabContent = selectedButton;
+    console.log(tabContent);
+  }
+
+  console.log('APP COMPONENT EXECUTING');
+
   return (
     <div>
       <Header/>
@@ -41,13 +24,22 @@ function App() {
           <CoreConcept
           title={CORE_CONCEPTS[0].title}
           description={CORE_CONCEPTS[0].description}
-          image={CORE_CONCEPTS[0].image}
+          image={CORE_CONCEPTS[0].image} 
           />
-        
           <CoreConcept {...CORE_CONCEPTS[1]} />
           <CoreConcept {...CORE_CONCEPTS[2]} />
           <CoreConcept {...CORE_CONCEPTS[3]} />
         </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {tabContent}
         </section>
       </main>
     </div>
